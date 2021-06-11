256a146132542067a5c5ee53227a5171a0fd030f
AlexisAlegria <alexisalegriav@gmail.com>
2021-06-11 21:35:36 +0000

    avance hasta ejer 10.1

--- exercises/08.3-Sum-all-items/app.js
+++ exercises/08.3-Sum-all-items/app.js
@@ -3,6 +3,11 @@ function sumTheElements(theArray){
 	var total = 0;
 	
 	//your code here
+    var theArray = [2,13,34,5];
+
+    for(i=0; i<theArray.length; i++){
+        total += theArray[i];
+    }
 
 	return total;
 }
