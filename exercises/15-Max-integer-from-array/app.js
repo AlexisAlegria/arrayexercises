36e942064f363b27456c3b60b6df042c2e78ce79
AlexisAlegria <alexisalegriav@gmail.com>
2021-06-13 00:54:02 +0000

    avance hasta ejer 16

--- exercises/15-Max-integer-from-array/app.js
+++ exercises/15-Max-integer-from-array/app.js
@@ -1,1 +1,11 @@
 var myArray = [43,23,6,87,43,1,4,6,3,67,8,3445,3,7,5435,63,346,3,456,734,6,34];
+
+let x = 0;
+
+for(i=0; i<myArray.length; i++){
+    let element = myArray[i];
+
+    if(x < element) x = element
+    else x = x;
+}
+console.log(x);
