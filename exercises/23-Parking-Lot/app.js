01733347297f6340b4915a9706e45291dc17ee0b
AlexisAlegria <alexisalegriav@gmail.com>
2021-06-15 21:48:09 +0000

    avance hasta ejer 25 FINISHED

--- exercises/23-Parking-Lot/app.js
+++ exercises/23-Parking-Lot/app.js
@@ -3,4 +3,24 @@ let parking_state = [
   [0,0,0],
   [1,1,2]
 ]
-console.log(getParkingLotState(parking_state))
+const getParkingLotState = (n) => {
+    let state = {
+      totalSlots: 0,
+      availableSlots: 0,
+      occupiedSlots: 0
+    }
+
+    for(i=0; i<n.length; i++){
+      for(x=0; x<n.length; x++){
+        if(n[i][x] == 2){
+          state.availableSlots += 1
+          state.totalSlots +=1
+        } else if(n[i][x] == 1) {
+          state.occupiedSlots += 1
+          state.totalSlots += 1
+        }
+      }  
+    }
+    return state;
+}
+console.log(getParkingLotState(parking_state));
--- exercises/25-Techno-beat/app.js
+++ exercises/25-Techno-beat/app.js
@@ -1,7 +1,20 @@
-
-// test Data
-console.log(lyricsGenerator([0,0,1,1,0,0,0]))
-console.log(lyricsGenerator([0,0,1,1,1,0,0,0]))
-console.log(lyricsGenerator([0,0,0]))
-console.log(lyricsGenerator([1,0,1]))
-console.log(lyricsGenerator([1,1,1]))
+const lyricsGenerator = (newArray) => {
+    let string = "";
+      for(i=0; i<newArray.length; i++){
+          if(newArray[i] === 0){
+            string += 'Boom '
+          } else if(newArray[i] === 1) {
+            string += 'Drop the Base '
+              if(newArray[i-1] === 1 && newArray[i-2] === 1)
+              string += '!!!Break the Base!!! '
+          } 
+      }
+      return string;  
+  }
+  
+  // test Data
+  console.log(lyricsGenerator([0,0,1,1,0,0,0]))
+  console.log(lyricsGenerator([0,0,1,1,1,0,0,0]))
+  console.log(lyricsGenerator([0,0,0]))
+  console.log(lyricsGenerator([1,0,1]))
+  console.log(lyricsGenerator([1,1,1]))
